359801e79e67230850d12a06b8991263a7d781f3
asison19 <andrewsison19@gmail.com>
2018-10-01 22:04:33 -0700

    Added Comments

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,18 +1,21 @@
 #sudoku solver in python
 #Name: Andrew Sison
 
+#returns true if the number is in the row
 def isInRow(sudoku, rowNumber, number):
 	for i in range(sudokuDimension):
 		if (int(sudoku[rowNumber][i]) == number):
 			return True
 	return False
 
+#returns true if the number is in the column
 def isInColumn(sudoku, columnNumber, number):
 	for i in range(sudokuDimension):
 		if (int(sudoku[i][columnNumber]) == number):
 			return True
 	return False
 
+#returns true if the number is in the local 3x3 block
 def isInBlock(sudoku, rowNumber, columnNumber, number):
 	r = rowNumber - (rowNumber % 3)
 	c = columnNumber - (columnNumber % 3)
@@ -23,9 +26,11 @@ def isInBlock(sudoku, rowNumber, columnNumber, number):
 				return True
 	return False
 
+#checks rows, columns, and local block
 def isValid(sudoku, row, column, number):
 	return not isInRow(sudoku, row, number) and not isInColumn(sudoku, column, number) and not isInBlock(sudoku, row, column, number)
 
+#Actual algorithm that solves the sudoku
 def isSolved(sudoku, emptyMarker):
 	for i in range(sudokuDimension):
 		for j in range(sudokuDimension):
@@ -40,28 +45,32 @@ def isSolved(sudoku, emptyMarker):
 				return False
 	return True
 
+#reads in sudoku file, and puts it into the 2d sudoku array
 def readInSudoku(sudokuFile, sudokuDimension, sudoku):
 	for i in range(sudokuDimension):
 		row = sudokuFile.readline().split(' ')
 		for j in range(sudokuDimension):
 			sudoku[i][j] = row[j]
 
+#output the sudoku
 def outputSudoku(sudokuDimension):
 	for i in range(sudokuDimension):
 		for j in range(sudokuDimension):
 			print(sudoku[i][j], end="")
 
+
 sudokuDimension = 9
-sudokuFile = open("sudokuTest - Copy.txt","r")
+sudokuFile = open("sudokuTest.txt","r")
 emptyMarker = "0"
 
-#instantiate empty sudoku 2d array
+#create empty sudoku 2d array
 sudoku = [[0 for x in range(sudokuDimension)] for y in range(sudokuDimension)]
+readInSudoku(sudokuFile, sudokuDimension, sudoku) #give empty sudoku 2d array the proper values
 
-readInSudoku(sudokuFile, sudokuDimension, sudoku) #create sudoku
 print("Here is the Sudoku to solve: ")
-outputSudoku(sudokuDimension)
-if isSolved(sudoku, emptyMarker):
-	print("Here is the Solved Sudoku: ")
-	outputSudoku(sudokuDimension)
+outputSudoku(sudokuDimension) #output originalsudoku
 
+if isSolved(sudoku, emptyMarker): #this algorithm returns true once sudoku is solved
+	print("Here is the Solved Sudoku: ")
+	outputSudoku(sudokuDimension) #output solved sudoku
+#end of program
